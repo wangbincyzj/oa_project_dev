1a96d3d734f36783b714b7a71d517cec372296d5
wangbin <309567157@qq.com>
2020-07-06 16:52:22 +0800

    0706 mergecomp

--- src/views/menu_1/Home.vue
+++ src/views/menu_1/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  
+  <div class="home"></div>
 </template>
 
 <script>
@@ -8,6 +8,8 @@
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .home{
+    background: url("~@/assets/images/home/home.jpg");
+  }
 </style>
